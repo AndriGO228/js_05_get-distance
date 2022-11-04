7503c7580778596c09a36fe2671677ba2b75c4e8
andriGo <afomin129@gmail.com>
2022-11-04 03:40:12 +0200

    variables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getDistance(distanceTraveled, traveledPercent) {
-	// write your code here
+	const allDistanceTraveled = distanceTraveled * (100 / traveledPercent);
+	return 'distance between village and capital: ' + String(allDistanceTraveled) + 'm';
 }
 
 module.exports = getDistance;
